c294e8368ea2371469f3d3e55922de441bd0f381
Al Amin <alamincmt7418@gmail.com>
2026-07-16 12:34:57 +0600

    working clipboard in localhost

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,11 +1,31 @@
 function bpgpbHandleShortcode(id) {
 	var input = document.querySelector('#bpgpbFrontShortcode-' + id + ' input');
 	var tooltip = document.querySelector('#bpgpbFrontShortcode-' + id + ' .tooltip');
-	input.select();
-	input.setSelectionRange(0, 30);
-	document.execCommand('copy');
-	tooltip.innerHTML = wp.i18n.__('Copied Successfully!', 'embed-google-photos');
-	setTimeout(function () {
-		tooltip.innerHTML = wp.i18n.__('Copy To Clipboard', 'embed-google-photos');
-	}, 1500);
+	var text = input.value;
+
+	function showCopied() {
+		tooltip.innerHTML = wp.i18n.__('Copied Successfully!', 'embed-google-photos');
+		setTimeout(function () {
+			tooltip.innerHTML = wp.i18n.__('Copy To Clipboard', 'embed-google-photos');
+		}, 1500);
+	}
+
+	if (navigator.clipboard) {
+		navigator.clipboard.writeText(text).then(showCopied).catch(fallbackCopy);
+	} else {
+		fallbackCopy();
+	}
+
+	function fallbackCopy() {
+		input.select();
+		input.setSelectionRange(0, 99999);
+		try {
+			var ok = document.execCommand('copy');
+			if (ok) {
+				showCopied();
+			}
+		} catch (err) {
+			// failed
+		}
+	}
 }
